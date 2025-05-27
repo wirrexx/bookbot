8b989827f2cef4efc2b6c522c0831822994b0360
Wisam Odish <wirrexx@gmail.com>
2025-05-27 15:16:05 +0200

    C: Bookbot

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("greetings boots")
+def get_book_text(filepath):
+    with open(filepath, 'r', encoding='utf-8') as file:
+        return file.read()
+
+def main():
+    text = get_book_text("books/frankenstein.txt")
+    print(text)
+
+main()

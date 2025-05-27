551092dd69fac29913e0b4bf90bd1e8eb58d14b1
Wisam Odish <wirrexx@gmail.com>
2025-05-27 18:37:03 +0200

    G: stats.py

--- stats.py
+++ stats.py
@@ -1,3 +1,11 @@
+from collections import Counter
+def get_book_text(filepath):
+    with open(filepath, 'r', encoding='utf-8') as file:
+        return file.read()
+
+def main():
+    text = get_book_text("books/frankenstein.txt")
+    print(text)
 
 
 def count_words(text):
@@ -5,3 +13,20 @@ def count_words(text):
 
     count=len(split_word)
     print(f"{count} words found in the document")
+
+
+def count_chars(text):
+    
+    counted_text = Counter(text.lower())
+    print(counted_text)
+
+            
+        
+        
+        
+    
+
+counted_book = get_book_text("books/frankenstein.txt")
+# count_words(text)
+count_chars(counted_book)
+

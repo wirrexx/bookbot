3b0a6bda8a098182cd376bd919e07233c6bea35f
Wisam Odish <wirrexx@gmail.com>
2025-05-27 18:36:52 +0200

    F: main.py

--- main.py
+++ main.py
@@ -1,13 +1,1 @@
 from stats import count_words
-
-def get_book_text(filepath):
-    with open(filepath, 'r', encoding='utf-8') as file:
-        return file.read()
-
-def main():
-    text = get_book_text("books/frankenstein.txt")
-    print(text)
-
-
-text = get_book_text("books/frankenstein.txt")
-count_words(text)

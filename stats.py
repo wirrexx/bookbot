6df7db31ec6ec0cf4589a0fd0a103fe21d02cb6e
Wisam Odish <wirrexx@gmail.com>
2025-05-27 20:31:50 +0200

    H: stats.py

--- stats.py
+++ stats.py
@@ -3,30 +3,32 @@ def get_book_text(filepath):
     with open(filepath, 'r', encoding='utf-8') as file:
         return file.read()
 
-def main():
-    text = get_book_text("books/frankenstein.txt")
-    print(text)
-
-
 def count_words(text):
     split_word = text.split()
-
     count=len(split_word)
-    print(f"{count} words found in the document")
-
+    return count
 
 def count_chars(text):
     
-    counted_text = Counter(text.lower())
-    print(counted_text)
+    char_counts = Counter(char.lower() for char in text if char.isalpha())
+    return char_counts.most_common()
 
             
         
-        
-        
+def sort_on(filepath):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {filepath}...")
     
+    text = get_book_text(filepath)
+    
+    print("----------- Word Count ----------")
+    print(f"Found {count_words(text)} total words")
+    
+    print("--------- Character Count -------")
+    for char, count in count_chars(text):
+        print(f"{char}: {count}")
+    
+    print("============= END ===============")
 
-counted_book = get_book_text("books/frankenstein.txt")
-# count_words(text)
-count_chars(counted_book)
 
+sort_on("books/frankenstein.txt")
